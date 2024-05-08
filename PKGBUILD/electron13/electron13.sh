8370d9e2dcf6f00d8b7f73728b3adcf6c7a04cb3
imxieke <oss@live.hk>
2024-05-09 00:08:08 +0800

    init

---
+++ PKGBUILD/electron13/electron13.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+###
+ # @Author: Cloudflying
+ # @Date: 2022-09-21 09:03:57
+ # @LastEditTime: 2022-09-21 09:27:13
+ # @LastEditors: Cloudflying
+ # @Description: Electron Launcher
+ # @FilePath: electron15.sh
+### 
+
+set -euo pipefail
+
+name=electron13
+flags_file="${XDG_CONFIG_HOME:-$HOME/.config}/${name}-flags.conf"
+
+declare -a flags
+
+if [[ -f "${flags_file}" ]]; then
+    mapfile -t < "${flags_file}"
+fi
+
+for line in "${MAPFILE[@]}"; do
+    if [[ ! "${line}" =~ ^[[:space:]]*#.* ]]; then
+        flags+=("${line}")
+    fi
+done
+
+exec /usr/lib/${name}/electron "$@" "${flags[@]}"
